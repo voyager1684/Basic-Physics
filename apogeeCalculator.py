b6e65292a15b023b41b8be9682bdfa4e6b4cd529
voyager1684 <84234714+voyager1684@users.noreply.github.com>
2021-07-16 13:48:20 +0300

    Add files via upload

---
+++ apogeeCalculator.py
@@ -0,0 +1,35 @@
+Python 3.9.5 (v3.9.5:0a7dcbdb13, May  3 2021, 13:17:02) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> g = 9.81
+speed = 0
+alt = 0
+apogee = 0
+index = 0
+k = 0
+
+thrust = float(input("Enter the known thrust (in Newtons): "))
+mass = float(input("Enter the known mass (in kilograms): "))
+propellantMass = float(input("Enter the known propellant mass (in kilograms): "))
+burnRate = float(input("Enter the known burn rate (in kg/s): "))
+index = float(input("Input the interval (dt) for the integral.\nEnter a value less than 0.005 for a better approximation: "))
+
+
+while speed >= 0 and mass != propellantMass and alt >= 0:
+    k = k + 1
+    alt = alt + (speed*index + 0.5 * (thrust/(mass-(burnRate*index)) - g) * (index**2))
+    speed = speed + (thrust/(mass-(burnRate*index)) - g)*index
+    mass = mass - burnRate*index
+
+apogee = alt + (speed**2/(2*g))
+time = abs(speed/g)
+
+print("The apogee is ", str(round(alt + apogee)), "m,"," achieved in the ", str(round(k*0.005 + time)),
+      "th second. (", str(round((k*0.005 + time)/60)), "th minute.)")
+print("The thrust apogee is ", str(round(alt)), "m")
+print("\nDrag neglected.")
+
+
+
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
