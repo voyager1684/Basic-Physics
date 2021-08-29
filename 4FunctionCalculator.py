182bc68f20158f57cfba2491de783f569334e3ff
Alphan Umman <84234714+voyager1684@users.noreply.github.com>
2021-08-29 14:12:46 +0300

    Update 4FunctionCalculator.py

--- 4FunctionCalculator.py
+++ 4FunctionCalculator.py
@@ -1,7 +1,7 @@
-prob = input("Enter the operation (Put a space between each element): \n")          
-num1 = float(prob[0:prob.find(" ")])
-operator = str(prob[prob.find(" ") + 1 : prob.find(" ") + 2])
-num2 = float(prob[prob.find(" ") + 3:])
+problem = input("Enter the operation (Put a space between each element): \n")          
+num1 = float(problem[0:problem.find(" ")])
+operator = str(problem[problem.find(" ") + 1 : problem.find(" ") + 2])
+num2 = float(problem[problem.find(" ") + 3:])
 
 if operator == "+":
     print(num1+num2)
